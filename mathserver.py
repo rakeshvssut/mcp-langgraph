6ddf98d57fcff30d753e0e0947c53486a3c3d431
Rakesh Kumar Sahu <rakesh.vssut2022@gmail.com>
2025-07-29 20:46:46 +0530

    Update mathserver.py

--- mathserver.py
+++ mathserver.py
@@ -1,28 +1,22 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp= FastMCP("Math")
+mcp=FastMCP("Math")
 
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Adds two integers."""
-    return a + b
+def add(a:int,b:int)->int:
+    """_summary_
+    Add to numbers
+    """
+    return a+b
 
 @mcp.tool()
-def subtract(a: int, b: int) -> int:
-    """Subtracts the second integer from the first."""
-    return a - b
+def multiple(a:int,b:int)-> int:
+    """Multiply two numbers"""
+    return a*b
 
-@mcp.tool()
-def multiply(a: int, b: int) -> int:
-    """Multiplies two integers."""
-    return a * b
+#The transport="stdio" argument tells the server to:
 
-@mcp.tool()
-def divide(a: int, b: int) -> float:
-    """Divides the first integer by the second."""
-    if b == 0:
-        return 0
-    return a / b
+#Use standard input/output (stdin and stdout) to receive and respond to tool function calls.
 
-if __name__ == "__main__":
+if __name__=="__main__":
     mcp.run(transport="stdio")
